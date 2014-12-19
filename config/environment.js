276842c23f8b8c4754e3b5723ea7616c447a7dde
Alexander Bradner <alex@bradner.net>
2014-12-19 19:05:51 +1100

    Pods

--- config/environment.js
+++ config/environment.js
@@ -1,8 +1,21 @@
 /* jshint node: true */
 
+var os     = require('os');
+var ifaces = os.networkInterfaces();
+
+var addresses = [];
+for (var dev in ifaces) {
+  ifaces[dev].forEach(function(details){
+    if(details.family === 'IPv4' && details.address !== '127.0.0.1') {
+      addresses.push(details.address);
+    }
+  });
+}
+
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'freshmate',
+    podModulePrefix: 'freshmate/pods',
     environment: environment,
     baseURL: '/',
     locationType: 'hash',
@@ -10,6 +23,21 @@ module.exports = function(environment) {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
+        'ember-htmlbars': true,
+        'ember-routing-named-substates': true,
+        'composable-computed-properties': true,
+        'ember-routing-will-change-hooks': true,
+        'ember-metal-is-present': true,
+        'property-brace-expansion-improvement': true,
+        'ember-routing-multi-current-when': true,
+        'ember-runtime-item-controller-inline-class': true,
+        'ember-routing-fire-activate-deactivate-events': true,
+        'ember-testing-pause-test': true,
+        'ember-htmlbars-component-generation': true,
+        'ember-htmlbars-inline-if-helper': true,
+        'ember-htmlbars-attribute-syntax': true,
+        'ember-metal-injected-properties': true,
+        'ember-htmlbars-block-params': true
       }
     },
 
@@ -20,9 +48,16 @@ module.exports = function(environment) {
 
     cordova: {
       rebuildOnChange: false,
-      emulate: false
+      emulate: false,
+      emberUrl: 'http://' + addresses[0] + ':4200',
+      liveReload: {
+        enabled: false,
+        platform: 'ios'
+      }
     }
   };
+
+  ENV.GOOGLE_APP_ID = "187238474080-5onaooathckn0upobhsfo0k6g5qc0arj.apps.googleusercontent.com"
 
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
@@ -30,6 +65,9 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     ENV.APP.LOG_VIEW_LOOKUPS = true;
+
+    ENV.apiUrl   = 'http://' + addresses[0] + ':3000/api/v1';
+    ENV.development = true;
   }
 
   if (environment === 'test') {
@@ -44,8 +82,15 @@ module.exports = function(environment) {
     ENV.APP.rootElement = '#ember-testing';
   }
 
-  if (environment === 'production') {
+  if (environment === 'staging') {
+    ENV.apiUrl = 'http://freshmate-staging.herokuapp.com/api/v1';
+    ENV.staging = true;
+  }
 
+
+  if (environment === 'production') {
+    ENV.apiUrl = 'http://freshmate.herokuapp.com/api/v1';
+    ENV.production = true;
   }
 
   return ENV;
